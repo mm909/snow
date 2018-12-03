5c5fb775e2e3e289543412e8cd7cdd6646285046
Mikian Musser <Bobar312@gmail.com>
2018-12-03 14:37:44 -0800

    Created winter lib example

---
+++ type.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+  var type1 = new Typed('#type1', {
+    strings: [
+      ' <h1>This is an example of typed.js</h1>',
+      ' <h1>The snowflakes are an example of particles.js</h1>',
+      ' <h1>Above is an example of countup.js</h1>',
+      ' <h1>Want to learn how to add these to your website?</h1>',
+      ' <h1>Ask an instructor!</h1>',
+      ' <h1 class="cc"> #Code{Central} </h1>'
+    ],
+    typeSpeed: 10,
+    backSpeed: 10,
+    backDelay: 3000,
+    loop: true,
+  });
+});
